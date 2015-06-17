83281b69fc03c56ff979259e50b2bc8782ae82e5
Henry Phan <hphan.cs@gmail.com>
2015-06-17 11:40:57 -0400

    Add compatibility for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
 		@user = User.new(email: params[:email],
 												password: passhash)
 		if @user.save
-			render json: { user: @user }
+			#render json: "register.json.jbuilder, status: :created
+			render json: { user: @user.as_json(only: [:id, :email, :access_token]) }, status: :created
 		else
-			render json: {errors: @user.errors.full_messages}
+			render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
 		end
 
 
